e766565c478bf0ef5a2b4a056a98feddb7d729bc
Wan Nabil <wannabil.xyz@gmail.com>
2023-05-05 13:07:19 +0800

    final-003

--- src/App.js
+++ src/App.js
@@ -1,53 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react'
-
-
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(15);
+  const [temperature, setTemperature] = useState(15);
+
+  useEffect(() => {
+    if (temperature > 39) {
+      document.body.style.backgroundColor = "#d92808";
+    } else if (temperature > 34) {
+      document.body.style.backgroundColor = "#DF9210";
+    } else if (temperature > 29) {
+      document.body.style.backgroundColor = "#df6a10";
+    } else if (temperature > 24) {
+      document.body.style.backgroundColor = "#ffd104";
+    } else if (temperature > 19) {
+      document.body.style.backgroundColor = "#f5d328";
+    } else if (temperature > 14) {
+      document.body.style.backgroundColor = "#70bf40";
+    } else if (temperature > 9) {
+      document.body.style.backgroundColor = "#00fdff";
+    } else if (temperature > 4) {
+      document.body.style.backgroundColor = "#50a7f9";
+    } else {
+      document.body.style.backgroundColor = "#0432ff";
+    }
+  }, [temperature]);
+
+  const resetTemp = () => {
+    setTemperature(15);
+  };
+
+  const increaseByTwo = () => {
+    setTemperature(temperature + 2);
+  };
+
+  const decreaseByTwo = () => {
+    setTemperature(temperature - 2);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {
-        <><div className="btn-group" role="group" aria-label="Basic mixed styles example">
-          <div><h2>The temperature is now {count} degree Celcius</h2></div>
-          </div>
-          <button className="btn btn-outline-danger" onClick={() => setCount(count + 2)}>Increase by 2</button>
-          <button className="btn btn-outline-primary" onClick={() => setCount(count - 2)}>Decrease by 2</button>
-          <button className="btn btn-outline-success" onClick={() => setCount(15)}>Reset to 15</button></>
-
-    }
+        <h1>Temperature Control App</h1>
+        <p>Current Temperature: {temperature} °C</p>
+        <button className="btn btn-danger" onClick={increaseByTwo}>+2</button>
+        <button className="btn btn-primary" onClick={decreaseByTwo}>-2</button>
+        <button className="btn btn-success"onClick={resetTemp}>Reset</button>
+        <div className="btn-group" role="group" aria-label="Basic mixed styles example">
+          <div><h2>The temperature is now {temperature} degree Celcius</h2></div>
+        </div>
       </header>
     </div>
   );
-
- 
-}
-document.body.style.backgroundColor = "#70bf40";
-const TemperatureControl = useState;
-
-if (TemperatureControl > 39) {
-  document.body.style.backgroundColor = "#d92808";
-} else if (TemperatureControl > 34 ) {
-  document.body.style.backgroundColor = "#DF9210";
-} else if (TemperatureControl > 29) {
-  document.body.style.backgroundColor = "#df6a10";
-} else if (TemperatureControl > 24 ) {
-  document.body.style.backgroundColor = "#ffd104";
-} else if (TemperatureControl > 19 ) {
-  document.body.style.backgroundColor = "#f5d328";
-} else if (TemperatureControl> 14 ) {
-  document.body.style.backgroundColor = "#70bf40";
-} else if (TemperatureControl > 9 ) {
-  document.body.style.backgroundColor = "#00fdff";
-} else if (TemperatureControl > 4 ) {
-  document.body.style.backgroundColor =  "#50a7f9";
-} else {
-  document.body.style.backgroundColor = "#0432ff";
-
 }
 
 export default App;
